c750e80126eb52fb1b7589fabd468546911b06c6
codder077 <120477792+codder077@users.noreply.github.com>
2024-05-15 14:27:38 +0530

    body parcer

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const {PORT}=require('./config/serverconfig');
+
+const setup = async()=>{
+    const app=express();
+
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+}
+
+
+setup();
